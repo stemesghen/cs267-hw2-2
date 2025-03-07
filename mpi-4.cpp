c4c82e0880280451347ff3e640280230bba3f5c0
Sabrina Temesghen <stemesghen@berkeley.edu>
2025-03-06 20:23:47 -0500

    Update mpi-4.cpp

--- mpi-4.cpp
+++ mpi-4.cpp
@@ -1,4 +1,5 @@
 
+
 #include "common.h"
 #include <mpi.h>
 #include <cmath>
@@ -86,31 +87,31 @@ void simulate_one_step(particle_t* parts, int num_parts, double size, int rank, 
         if (parts[i].y > size - cutoff) send_bottom.push_back(parts[i]);
     }
 
-    int send_counts[2] = {static_cast<int>(send_top.size()), 
+    int send_counts[2] = {static_cast<int>(send_top.size()),
                           static_cast<int>(send_bottom.size())};
     int recv_counts[2];
 
-    MPI_Sendrecv(&send_counts[0], 1, MPI_INT, top_rank, 1, 
-                 &recv_counts[1], 1, MPI_INT, bottom_rank, 1, 
+    MPI_Sendrecv(&send_counts[0], 1, MPI_INT, top_rank, 1,
+                 &recv_counts[1], 1, MPI_INT, bottom_rank, 1,
                  MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-    MPI_Sendrecv(&send_counts[1], 1, MPI_INT, bottom_rank, 2, 
-                 &recv_counts[0], 1, MPI_INT, top_rank, 2, 
+    MPI_Sendrecv(&send_counts[1], 1, MPI_INT, bottom_rank, 2,
+                 &recv_counts[0], 1, MPI_INT, top_rank, 2,
                  MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
     recv_top.resize(recv_counts[0]);
     recv_bottom.resize(recv_counts[1]);
 
-    MPI_Sendrecv(send_top.data(), send_counts[0], PARTICLE, top_rank, 3, 
-                 recv_bottom.data(), recv_counts[1], PARTICLE, bottom_rank, 3, 
+    MPI_Sendrecv(send_top.data(), send_counts[0], PARTICLE, top_rank, 3,
+                 recv_bottom.data(), recv_counts[1], PARTICLE, bottom_rank, 3,
                  MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-    MPI_Sendrecv(send_bottom.data(), send_counts[1], PARTICLE, bottom_rank, 4, 
-                 recv_top.data(), recv_counts[0], PARTICLE, top_rank, 4, 
+    MPI_Sendrecv(send_bottom.data(), send_counts[1], PARTICLE, bottom_rank, 4,
+                 recv_top.data(), recv_counts[0], PARTICLE, top_rank, 4,
                  MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
     // ghost partical combined
-    std::vector<particle_t> all_particles(parts, parts + num_parts);
+      std::vector<particle_t> all_particles(parts, parts + num_parts);
     all_particles.insert(all_particles.end(), recv_top.begin(), recv_top.end());
     all_particles.insert(all_particles.end(), recv_bottom.begin(), recv_bottom.end());
 
@@ -124,32 +125,34 @@ void simulate_one_step(particle_t* parts, int num_parts, double size, int rank, 
         move(parts[i], size);
     }
 }
-
-
 void gather_for_save(particle_t* parts, int num_parts, double size, int rank, int num_procs) {
     std::vector<int> recv_counts(num_procs);
     std::vector<int> displs(num_procs);
 
     int local_size = num_parts;
 
+    // Gather local sizes
     MPI_Gather(&local_size, 1, MPI_INT, recv_counts.data(), 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    std::vector<particle_t> all_particles; // Declare here to fix error
 
     if (rank == 0) {
         displs[0] = 0;
         for (int i = 1; i < num_procs; i++) {
             displs[i] = displs[i - 1] + recv_counts[i - 1];
         }
+        all_particles.resize(displs[num_procs - 1] + recv_counts[num_procs - 1]); // Allocate space
     }
 
+    // Gather particles to rank 0
     MPI_Gatherv(parts, num_parts, PARTICLE,
-                rank == 0 ? parts : nullptr, recv_counts.data(), displs.data(), PARTICLE,
+                rank == 0 ? all_particles.data() : nullptr, recv_counts.data(), displs.data(), PARTICLE,
                 0, MPI_COMM_WORLD);
 
-
+    // Sort only on rank 0
     if (rank == 0) {
         std::sort(all_particles.begin(), all_particles.end(), [](const particle_t &a, const particle_t &b) {
             return a.id < b.id;
         });
-}
-
+    }
 }
